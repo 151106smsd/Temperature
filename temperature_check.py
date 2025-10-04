bd1967d4b06ac7abf7325a7f497f136408052be6
151106smsd <amokshagna7@gmail.com>
2025-10-04 09:54:43 +0530

    save

--- temperature_check.py
+++ temperature_check.py
@@ -2,7 +2,12 @@
 
 temp_c = float(input("Enter temperature in Celsius: "))
 
+# Celsius check
 if temp_c > 30:
     print("It's hot!")
 else:
     print("Temperature is normal.")
+
+# Convert to Fahrenheit
+temp_f = (temp_c * 9/5) + 32
+print(f"Equivalent Fahrenheit: {temp_f:.2f}°F")
